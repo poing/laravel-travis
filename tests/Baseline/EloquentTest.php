3b0a9cb427f97e0dae8c898b76f64a5e1607a214
Brian LaVallee <brian.lavallee@invite-comm.jp>
2020-04-18 15:41:59 +0000

    Apply fixes from StyleCI

--- tests/Baseline/EloquentTest.php
+++ tests/Baseline/EloquentTest.php
@@ -3,16 +3,13 @@
 namespace Matrix\Tests\Baseline;
 
 use Matrix\Tests\Baseline\Models\BaseLineAlpha;
-use Matrix\Tests\Baseline\UnitAbstract;
 
 class EloquentTest extends UnitAbstract
 {
-
-	/** @test */
+    /** @test */
     public function record_count_test()
     {
-		factory(BaseLineAlpha::class, 50)->create();
-		$this->assertEquals(BaseLineAlpha::count(), 50);
-	}
-
+        factory(BaseLineAlpha::class, 50)->create();
+        $this->assertEquals(BaseLineAlpha::count(), 50);
+    }
 }
